b80979491ccf0a8a54e006c75d80dde17774d4f8
sixtus6 <xystuseton@gmail.com>
2023-10-16 12:49:38 +0100

    saved refactored changes

--- lib/screens/onborading/index.dart
+++ lib/screens/onborading/index.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lingb/config/color.dart';
 import 'package:nb_utils/nb_utils.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -11,10 +13,28 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: ColorConfig.black, // Change the status bar color
+      statusBarIconBrightness:
+          Brightness.light, // Change the status bar icons' color
+      systemNavigationBarColor: Colors.transparent,
+      //   ColorConfig.black, // Change the navigation bar color
+      systemNavigationBarIconBrightness:
+          Brightness.light, // Change the navigation bar icons' color
+    ));
     return Scaffold(
       body: Container(
-        decoration: boxDecorationWithRoundedCorners(),
-        
+        height: 200,
+        width: double.infinity,
+        decoration: boxDecorationWithRoundedCorners(
+          backgroundColor: Colors.amber,
+          decorationImage: DecorationImage(
+              opacity: 0.7,
+              image: AssetImage("assets/images/background/background.png"),
+              fit: BoxFit.cover),
+
+        ),
+
         // padding: EdgeInsets.all(8),
         child: Column(
           children: [],

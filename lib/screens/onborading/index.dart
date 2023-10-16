ac98e7a1e500a5324322917f3d2932d615174189
sixtus6 <xystuseton@gmail.com>
2023-10-16 10:42:13 +0100

    changed the default splash screen color for my color theme

--- lib/screens/onborading/index.dart
+++ lib/screens/onborading/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -10,6 +11,15 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: boxDecorationWithRoundedCorners(),
+        
+        // padding: EdgeInsets.all(8),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }

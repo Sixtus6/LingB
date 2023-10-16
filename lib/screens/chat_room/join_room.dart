bfdbb824c3050f479017648010b11fd313935522
sixtus6 <xystuseton@gmail.com>
2023-10-16 16:50:48 +0100

    saved refactored changes

--- lib/screens/chat_room/join_room.dart
+++ lib/screens/chat_room/join_room.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lingb/config/color.dart';
 
 class JoinRoomScreen extends StatefulWidget {
   const JoinRoomScreen({super.key});
@@ -10,6 +12,15 @@ class JoinRoomScreen extends StatefulWidget {
 class _JoinRoomScreenState extends State<JoinRoomScreen> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: ColorConfig.black, // Change the status bar color
+      statusBarIconBrightness:
+          Brightness.light, // Change the status bar icons' color
+      systemNavigationBarColor:
+          ColorConfig.black, // Change the navigation bar color
+      systemNavigationBarIconBrightness:
+          Brightness.light, // Change the navigation bar icons' color
+    ));
     return Scaffold();
   }
 }

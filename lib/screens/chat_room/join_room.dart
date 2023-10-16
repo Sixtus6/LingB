ebd213f734b9d95634b3c70675fe470ddc2daeef
sixtus6 <xystuseton@gmail.com>
2023-10-16 16:47:43 +0100

    saved refactored changes

---
+++ lib/screens/chat_room/join_room.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JoinRoomScreen extends StatefulWidget {
+  const JoinRoomScreen({super.key});
+
+  @override
+  State<JoinRoomScreen> createState() => _JoinRoomScreenState();
+}
+
+class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

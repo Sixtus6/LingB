129ffc1025ff7e6e1113f2c166ed9bc244ef85f6
sixtus6 <xystuseton@gmail.com>
2023-10-17 07:42:55 +0100

    completed  the button for dropdown

--- lib/widget/dropdown.dart
+++ lib/widget/dropdown.dart
@@ -4,6 +4,7 @@ import 'package:lingb/config/size.dart';
 import 'package:lingb/screens/join_chat_room/provider.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:provider/provider.dart';
+import 'package:shimmer/shimmer.dart';
 
 class DropdownMenuWithDecoration extends StatelessWidget {
   @override
@@ -12,17 +13,25 @@ class DropdownMenuWithDecoration extends StatelessWidget {
       builder: (context, dropdownModel, _) {
         return Container(
           padding: EdgeInsets.only(
-              left: SizeConfigs.getPercentageWidth(12),
-              right: SizeConfigs.getPercentageWidth(12)),
+              left: SizeConfigs.getPercentageWidth(13),
+              right: SizeConfigs.getPercentageWidth(13)),
           decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(
+              color: ColorConfig.secondary.withOpacity(0.5),
+            ),
+            borderRadius: BorderRadius.circular(25.0),
           ),
           child: DropdownButtonHideUnderline(
             child: DropdownButton<String>(
+              icon: Shimmer.fromColors(
+                  baseColor: ColorConfig.primary,
+                  highlightColor: ColorConfig.white,
+                  child: Icon(
+                    Icons.arrow_drop_down,
+                  )),
               value: dropdownModel.selectedItem,
               iconEnabledColor: ColorConfig.primary,
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(15),
               onChanged: (String? newValue) {
                 if (newValue != null) {
                   dropdownModel.setSelectedItem(newValue);
@@ -53,7 +62,7 @@ class DropdownMenuWithDecoration extends StatelessWidget {
 // List of items for the dropdown menu
 List<String> _items = [
   'Select your preferred language',
-  'Item 2',
-  'Item 3',
-  'Item 4'
+  'Hausa',
+  'Igbo',
+  'French'
 ];

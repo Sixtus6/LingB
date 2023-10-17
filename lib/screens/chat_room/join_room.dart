cc0bccba9d7ea512f34fd95681ee84a907d0dd9c
sixtus6 <xystuseton@gmail.com>
2023-10-17 04:47:41 +0100

    saved refactored changes

--- lib/screens/chat_room/join_room.dart
+++ lib/screens/chat_room/join_room.dart
@@ -4,6 +4,7 @@ import 'package:lingb/config/color.dart';
 import 'package:lingb/config/size.dart';
 import 'package:lottie/lottie.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:shimmer/shimmer.dart';
 
 class JoinRoomScreen extends StatefulWidget {
   const JoinRoomScreen({super.key});
@@ -29,33 +30,37 @@ class _JoinRoomScreenState extends State<JoinRoomScreen> {
           body: SingleChildScrollView(
         child: Column(
           children: [
-            SizeConfigs.getPercentageWidth(1).toInt().height,
+            SizeConfigs.getPercentageWidth(2).toInt().height,
             Lottie.asset(
               "assets/lottie/onboard1.json",
             ),
             Padding(
               padding: EdgeInsets.all(SizeConfigs.getPercentageWidth(8)),
-              child: const Text(
-                "Join Chat Room",
-                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+              child: Shimmer.fromColors(
+                baseColor: Colors.white,
+                highlightColor: Colors.grey.shade600,
+                child: Text(
+                  "Join Chat Room",
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                ),
               ),
             ),
             Container(
               decoration: boxDecorationWithRoundedCorners(
-                backgroundColor: Colors.white,
+                backgroundColor: Colors.white10.withOpacity(0.1),
+                // decorationImage: DecorationImage(
+                //     opacity: 0.7,
+                //     image: AssetImage("assets/icon/lingb.png"),
+                //     fit: BoxFit.cover),
                 borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(55),
                     topRight: Radius.circular(55)),
-                border: Border(
-                  top: BorderSide(
-                    color: Colors.red, // Color of the top border line
-                    width: 2.0,
-
-                    // Thickness of the top border line
-                  ),
+                border: Border.all(
+                  color: Colors.transparent, // Color of the border line
+                  width: 0.5, // Thickness of the border line
                 ),
               ),
-              height: SizeConfigs.getPercentageWidth(134),
+              height: SizeConfigs.getPercentageWidth(133),
             ),
           ],
         ),
@@ -64,6 +69,27 @@ class _JoinRoomScreenState extends State<JoinRoomScreen> {
   }
 }
 
+// child: Container(
+//                 margin: EdgeInsets.only(top: SizeConfigs.getPercentageWidth(1)),
+//                 decoration: const BoxDecoration(
+//                   color: Colors.transparent,
+//                   // backgroundColor: Colors.transparent,
+//                   image: DecorationImage(
+//                       opacity: 0.7,
+//                       image: AssetImage("assets/icon/lingb.png"),
+//                       fit: BoxFit.cover),
+//                   borderRadius: BorderRadius.only(
+//                       topLeft: Radius.circular(55),
+//                       topRight: Radius.circular(55)),
+
+//                   // border: Border.all(
+//                   //   color: Colors.red, // Color of the border line
+//                   //   width: 0.5, // Thickness of the border line
+//                   // ),
+//                 ),
+//               )
+//                   .withHeight(SizeConfigs.getPercentageWidth(133))
+//                   .withWidth(double.infinity),
 
 
 // Container(
@@ -80,3 +106,21 @@ class _JoinRoomScreenState extends State<JoinRoomScreen> {
 //           children: [],
 //         )),
 //       ),
+
+// Container(
+//               decoration: boxDecorationWithRoundedCorners(
+//                 backgroundColor: Colors.transparent,
+//                 decorationImage: DecorationImage(
+//                     opacity: 0.7,
+//                     image: AssetImage("assets/icon/lingb.png"),
+//                     fit: BoxFit.cover),
+//                 borderRadius: const BorderRadius.only(
+//                     topLeft: Radius.circular(55),
+//                     topRight: Radius.circular(55)),
+//                 border: Border.all(
+//                   color: Colors.red, // Color of the border line
+//                   width: 0.5, // Thickness of the border line
+//                 ),
+//               ),
+//               height: SizeConfigs.getPercentageWidth(133),
+//             ),

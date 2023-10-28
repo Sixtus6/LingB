0125b753e2d4f6cd2be39485adc31f803509d8dc
sixtus6 <xystuseton@gmail.com>
2023-10-28 13:57:10 +0100

    saved refactored changes

--- lib/config/socket/socket_method.dart
+++ lib/config/socket/socket_method.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:lingb/config/socket/socket_client.dart';
 
 class SocketMethods {
@@ -8,9 +9,21 @@ class SocketMethods {
     "join": "join-room",
     "chat": "chat-room"
   };
+
+  Map eventListeners = {
+    "create": ["create-room-msg"],
+    "join": ["join-room-msg", "join-room-error"],
+    "chat": ["chat-room-msg", "chat-room-error"]
+  };
   //create  room socket
   void createRoom() {
     _socket.emit(event["create"], {});
   }
-  
+
+  //create room listiner
+  void createRoomEvent(BuildContext context) {
+    _socket.on(eventListeners["create"], (data) {
+      print(data);
+    });
+  }
 }

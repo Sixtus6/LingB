15a0a705ef575e14d8ef9c7d58db9c693598ca8a
sixtus6 <xystuseton@gmail.com>
2023-10-28 13:25:47 +0100

    created socket room

--- lib/config/socket/socket_method.dart
+++ lib/config/socket/socket_method.dart
@@ -1,1 +1,16 @@
-cla
+import 'package:lingb/config/socket/socket_client.dart';
+
+class SocketMethods {
+  final _socket = SocketClient.instance.socket!;
+
+  Map event = {
+    "create": "create-room",
+    "join": "join-room",
+    "chat": "chat-room"
+  };
+  //create  room socket
+  void createRoom() {
+    _socket.emit(event["create"], {});
+  }
+  
+}
